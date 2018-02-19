e4e5e083dac91e1247e4d7c7af0e21f55755ae84
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-02-19 14:36:30 +0100

    AI gives unexpected error

--- app/src/main/java/com/example/jacco/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/jacco/tictactoe/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,9 +22,10 @@ public class MainActivity extends AppCompatActivity {
     int[] buttons = {R.id.t1, R.id.t2, R.id.t3, R.id.t4, R.id.t5, R.id.t6, R.id.t7,
                      R.id.t8, R.id.t9};
     int[][] coordinates = {{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
-    boolean gameOver;
     boolean AIOn;
     int randTile;
+    private int[] emptySet = {};
+    public int[] betterPlay;
 
     // saving state buttons
     @Override
@@ -31,85 +33,109 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        game = new Game();
-        gameOver = false;
-
         if (savedInstanceState != null) {
-            for (int i = 0; i < buttons.length; i++) {
+            game =(Game) savedInstanceState.getSerializable("Game");
+
+            for (int i=0; i<names.length; i++){
+                // kan niet meer winnen na draaaien en X begint altijd na draaien
+                Tile tile = game.drawTile(i/3, i%3);
+
                 Button button = findViewById(buttons[i]);
-                String state = button.getText().toString();
-                if (button != null) {
-                    button.setText(savedInstanceState.getString(state));
+                if (tile == Tile.CROSS) {
+                    button.setText("X");
+                } else if (tile == Tile.CIRCLE) {
+                    button.setText("O");
                 }
             }
+        } else {
+            game = new Game();
         }
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        for (int i = 0; i < buttons.length; i++) {
+
+         outState.putSerializable("Game",game);
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Game game =(Game) savedInstanceState.getSerializable("Game");
+
+        for (int i=0; i<names.length; i++){
+            // kan niet meer winnen na draaaien en X begint altijd na draaien
+            Tile tile = game.drawTile(i/3, i%3);
+
             Button button = findViewById(buttons[i]);
-            outState.putString(names[i], button.getText().toString());
+            if (tile == Tile.CROSS) {
+                button.setText("X");
+            } else if (tile == Tile.CIRCLE) {
+                button.setText("O");
+            }
         }
     }
 
     public void tileClicked(View view) {
         // find clicked button
-        if (gameOver == false) {
-            int id = view.getId();
+        int id = view.getId();
 
-            int length = buttons.length;
-            for (int i=0; i<length; i++) {
-                if (id == buttons[i]) {
-                    row = coordinates[i][0];
-                    column = coordinates[i][1];
-                }
+        int length = buttons.length;
+        for (int i=0; i<length; i++) {
+            if (id == buttons[i]) {
+                row = coordinates[i][0];
+                column = coordinates[i][1];
             }
+        }
 
-            // find out what to draw
-            Tile tile = game.draw(row, column);
+        // find out what to draw
+        Tile tile = game.draw(row, column);
 
-            // print on board
-            Button button = (Button) view;
-            switch(tile) {
-                case CROSS:
-                    button.setText("X");
-                    break;
-                case CIRCLE:
-                    button.setText("O");
-                    break;
-                case INVALID:
-                    // popup message from:
-                    // https://developer.android.com/guide/topics/ui/notifiers/toasts.html#Positioning
-                    Context context = getApplicationContext();
-                    CharSequence text = "Already selected!";
-                    int duration = Toast.LENGTH_SHORT;
+        // print on board
+        Button button = (Button) view;
+        switch(tile) {
+            case CROSS:
+                button.setText("X");
+                break;
+            case CIRCLE:
+                button.setText("O");
+                break;
+            case INVALID:
+                // popup message from:
+                // https://developer.android.com/guide/topics/ui/notifiers/toasts.html#Positioning
+                Context context = getApplicationContext();
+                CharSequence text = "Invalid move!";
+                int duration = Toast.LENGTH_SHORT;
 
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                    break;
-            }
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                break;
 
             // check if AI has to play
-            if (AIOn && game.process() != GameState.PLAYER_ONE) {
-                Tile tileTest;
-                // find a blank tile and find out what to draw
-                do {
-                    Random rand = new Random();
-                    randTile = rand.nextInt(9);
-                    tileTest = game.test(coordinates[randTile][0],coordinates[randTile][1]);
-                } while (tileTest == Tile.INVALID || row == coordinates[randTile][0] || column == coordinates[randTile][1]);
-                game.draw(coordinates[randTile][0],coordinates[randTile][1]);
-
-                // print on board
-                Button button2 = findViewById(buttons[randTile]);
-                button2.setText("O");
-            }
-
-            // check if game is over
-            if (game.process() != GameState.IN_PROGRESS) {
-                gameOver = true;
-            }
+//            if (AIOn && game.process() != GameState.PLAYER_ONE) {
+//                // check if there's a good play
+//
+//                if (game.prevent()<9) {
+//                    randTile = game.prevent();
+//                } else {
+//                    // find a random blank tile and find out what to draw
+//                    Tile tileTest;
+//                    do {
+//                        Random rand = new Random();
+//                        randTile = rand.nextInt(9);
+//                        tileTest = game.test(coordinates[randTile][0], coordinates[randTile][1]);
+//                    }
+//                    while (tileTest == Tile.INVALID || row == coordinates[randTile][0] || column == coordinates[randTile][1]);
+//                }
+//
+//                game.draw(coordinates[randTile][0],coordinates[randTile][1]);
+//
+//                // print on board
+//
+//                Button button2 = findViewById(buttons[randTile]);
+//                button2.setText("O");
+//            }
         }
 
         // find out who won and print message
@@ -144,7 +170,6 @@ public class MainActivity extends AppCompatActivity {
             button.setText("");
         }
         game = new Game();
-        gameOver = false;
     }
 
     public void switchCheck(View view) {
@@ -158,7 +183,6 @@ public class MainActivity extends AppCompatActivity {
                 button.setText("");
             }
             game = new Game();
-            gameOver = false;
         } else {
             AIOn = false;
         }
